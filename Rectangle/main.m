c4a7ce22f227d9a168b57743fb8c4030bc885bbc
Алексей Винокуров <vinokurov@vinokurov.local>
2011-07-20 14:29:04 +0400

    Initial Commit

---
+++ Rectangle/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Rectangle
+//
+//  Created by Алексей Винокуров on 20.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
